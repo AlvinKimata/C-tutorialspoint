93f45ca9f8c09451ee5a4beb4ae8f1f57badfa34
Alvin Kimata <alvinkimata@gmail.com>
2023-01-30 12:28:29 +0300

    arrays in C#

---
+++ Arrays/Program.cs
@@ -0,0 +1,35 @@
+﻿//Declaringm assigning and accessing arrays.
+using System;
+
+namespace Application
+{
+    class MyArray
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[10]; //Array of 10 integers.
+            int i, j;
+
+            //Init elements of array n.
+            for (i = 0; i < 10; i++)
+            {
+                n[i] = i + 100;
+            }
+
+            //Print elements of the array.
+            for (j = 0; j < 10; j++)
+            {
+                Console.WriteLine("Element [{0}] = {1}", j, n[j]);
+            }
+            Console.ReadKey();
+            Console.WriteLine("Using foreach loop. \n");
+
+            foreach (int j in n)
+            {
+                int i = j - 100;
+                Console.WriteLine("Element [{0}] = {1}", i, j);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Passing array as a function/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Application
+{
+    class MyArray
+    {
+        double getAverage(int[] arr, int size)
+        {
+            int i;
+            double avg;
+            int sum = 0;
+
+            for (i = 0; i < size; ++i)
+            {
+                sum += arr[i];
+            }
+            avg = (double)sum / size;
+            return avg;
+        }
+        static void Main(string[] args)
+        {
+            MyArray app = new MyArray();
+
+            //init array with 5 elements.
+            int[] balance = new int[] { 1000, 2, 4, 456, 7 };
+            double avg;
+            avg = app.getAverage(balance, 5);
+
+            Console.WriteLine("Average value is: {0}:", avg);
+            Console.ReadKey();
+        }
+    }
+}
