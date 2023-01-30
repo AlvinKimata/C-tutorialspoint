d03c14c51d112948a10dc93321a1804eaa9c3b0d
Alvin Kimata <alvinkimata@gmail.com>
2023-01-30 09:46:51 +0300

    data types in C#

---
+++ Datatypes/Program.cs
@@ -0,0 +1,15 @@
+﻿//Datatypes in C#.
+
+using System;
+
+namespace DataTypeApplication
+{
+    class Program
+    {
+        static void Main(string[] args) { 
+        Console.WriteLine("Size of int: {0}", sizeof(int));
+        Console.ReadLine();
+
+        }
+    }
+}
