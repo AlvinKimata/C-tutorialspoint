6596daec065c00e1bc9f0e5a868f6e23175d2b61
Alvin Kimata <alvinkimata@gmail.com>
2023-01-30 13:14:59 +0300

    inheritance and multiple inheritance in C#

---
+++ Inheritance/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace InheritanceApplication
+{
+    class Shape
+    {
+        public void setWidth(int w)
+        {
+            width = w;
+        }
+
+        public void setHeight(int h)
+        {
+            height = h;
+        }
+        protected int width;
+        protected int height;
+    }
+
+    //Derived class.
+    class Rectangle: Shape
+    {
+        public int getArea()
+        {
+            return (width * height);
+        }
+    }
+
+    class RectangleTester
+    {
+        static void Main(string[] args)
+        {
+            Rectangle Rect = new Rectangle();
+
+            Rect.setHeight(5);
+            Rect.setWidth(9);
+
+            //Print the area of the object.
+            Console.WriteLine("Total area: {0}", Rect.getArea());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Mutiple Inheritance/Program.cs
@@ -0,0 +1,56 @@
+﻿//C# does not implement multiple inheritance.
+//One can use interfaces to implement multiple inheritance.
+using System;
+
+namespace InheritanceApplication
+{
+    class Shape
+    {
+        public void setWidth(int w)
+        {
+            width = w;
+        }
+        public void setHeight(int h)
+        {
+            height = h;
+        }
+        protected int width;
+        protected int height;
+    }
+
+    //Base class PaintCost
+    public interface PaintCost
+    {
+        int getCost(int area);
+    }
+
+    //Derived class.
+    class Rectangle: Shape, PaintCost
+    {
+        public int getArea()
+        {
+            return (width * height);
+        }
+
+        public int getCost(int area)
+        {
+            return area * 70;
+        }
+    }
+
+    class RectangleTester
+    {
+        static void Main(string[] args)
+        {
+            Rectangle Rect = new Rectangle();
+            
+            Rect.setWidth(5);
+            Rect.setHeight(7);
+            int area = Rect.getArea();
+
+            //Print the area of the object.
+            Console.WriteLine("Total area: {0}", area);
+            Console.WriteLine("Total paint cost: ${0}", Rect.getCost(area));
+        }
+    }
+}
