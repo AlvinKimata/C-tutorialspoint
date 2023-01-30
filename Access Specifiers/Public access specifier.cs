41a1db41b401caf44f448b97e220946efc7aa96b
Alvin Kimata <alvinkimata@gmail.com>
2023-01-30 11:02:41 +0300

    Access specifiers in C#

---
+++ Access Specifiers/Public access specifier.cs
@@ -0,0 +1,37 @@
+﻿//Public specifiers in C#.
+using System;
+
+namespace RectangleApplication
+{
+    class Rectangle
+    {
+        //Member variables.
+        public double length;
+        public double width;
+
+        public double GetArea()
+        {
+            return length * width;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}", length);
+            Console.WriteLine("Width: {0}", width);
+
+            Console.WriteLine("Area: {0}", GetArea());
+        }
+    } //End class Rectangle.
+
+    class ExecuteRectangle
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle();
+            r.length = 10;
+            r.width = 3.5;
+            r.Display();
+            Console.ReadLine();
+        }
+    }
+}
