94566ff92cdffc59eda3b0abf89ca056a20ae04b
Alvin Kimata <alvinkimata@gmail.com>
2023-01-30 11:43:05 +0300

    nullables in C#

---
+++ Null coalescing operator/Program.cs
@@ -0,0 +1,24 @@
+﻿//Null coalescing operator (??) is used for converting an operand to the type of
+//anothe variable (or not) value type operand, where an implicit conversion is possible.
+
+using System;
+
+namespace CalculatorApplication
+{
+    class NullablesAtShow
+    {
+        static void Main(string[] args)
+        {
+            double? num1 = null;
+            double? num2 = 3.142;
+            double num3;
+
+            num3 = num1 ?? 5.34;
+            Console.WriteLine("Value of num3: {0}", num3);
+
+            num3 = num2 ?? 5.34;
+            Console.WriteLine("Value of num3: {0}", num3);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Nullables/Program.cs
@@ -0,0 +1,24 @@
+﻿//C# nullables.
+using System;
+
+namespace CalculatorApplication
+{
+    class NUllablesAtShow
+    {
+        static void Main(string[] args)
+        {
+            int? num1 = null;
+            int? num2 = 45;
+
+            double? num3 = new double?();
+            double? num4 = 3.147;
+
+            bool? boolval = new bool?();
+
+            //Display values.
+            Console.WriteLine("Nullables at Show: {0}, {1}, {2}, {3}", num1, num2, num3, num4);
+            Console.WriteLine("A nullable boolean value: {0}", boolval);
+            Console.ReadLine();
+        }
+    }
+}
