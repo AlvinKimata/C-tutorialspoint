936d5180aa753858e02b0672bd00e6ff95612d27
Alvin Kimata <alvinkimata@gmail.com>
2023-01-30 11:20:57 +0300

    methods and recursive methods

---
+++ Recursive Method call/Program.cs
@@ -0,0 +1,32 @@
+﻿//Recursion.
+using System;
+
+namespace CalculatorApplication
+{
+    class Numbermanipulation
+    {
+        public int factorial (int num)
+        {
+            int result;
+            if (num == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                result = factorial(num - 1) * num;
+                return result;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Numbermanipulation n = new Numbermanipulation();
+
+            //Call the factorial method.
+            Console.WriteLine("Factorial of 7 is: {0}", n.factorial(7));
+            Console.WriteLine("Factorial of 8 is: {0}", n.factorial(8));
+            Console.ReadLine();
+        }
+    }
+}
