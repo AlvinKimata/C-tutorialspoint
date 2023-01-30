3e740d885f8c4a86a282e5cb888feb2ed4ee9de1
Alvin Kimata <alvinkimata@gmail.com>
2023-01-30 10:42:49 +0300

    Loops in C#

---
+++ Operators/Program.cs
@@ -0,0 +1,27 @@
+﻿//While loop in C#
+using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            //Local variable definintion.
+            int a = 10;
+            
+            //While loop execution.
+            while (a < 20)
+            {
+                Console.WriteLine("Value of a: {0}", a);
+                a++;
+
+                if (a > 15)
+                {
+                    //Terminate the loop using a break statement.
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
