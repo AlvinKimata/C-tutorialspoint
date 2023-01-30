c77028f648651a9329ef6f3c256801323cb874c6
Alvin Kimata <alvinkimata@gmail.com>
2023-01-30 10:30:48 +0300

    variable declaration in C#

---
+++ Type Conversion/Program.cs
@@ -0,0 +1,19 @@
+﻿//Explicit type conversion in C#
+using System;
+
+namespace TypeConversionApplication
+{
+    class ExplicitConversion
+    {
+        static void Main(string[] args)
+        {
+            double d = 5673.74;
+            int i;
+
+            //Cast double to integer.
+            i = (int)d;
+            Console.WriteLine(i);
+            Console.ReadKey();
+        }
+    }
+}
